85f3b325ea5dc9e46e48b54593b37dc977f32939
soundarya dash <dashsoundarya@gmail.com>
2021-03-04 00:46:53 +0530

    added flutter projects

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  final Function addTx;
+  NewTransaction(this.addTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    addTx(enteredTitle, enteredAmount);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 8,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: titleController,
+                onSubmitted: (_) => submitData,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => submitData,
+              ),
+              FlatButton(
+                  onPressed: submitData,
+                  child: Text('Add Transaction'),
+                  textColor: Colors.purple)
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/widgets/user_transaction.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import './new_transaction.dart';
+import './transaction_list.dart';
+import '../models/transaction.dart';
+
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekely Groceries',
+        amount: 16.99,
+        date: DateTime.now())
+  ];
+
+  void _addNewTransactions(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(_addNewTransactions),
+        TransactionList(_userTransaction)
+      ],
+    );
+  }
+}
